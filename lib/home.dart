0cb0920f5e82f1c522ccfa9ba3cea1532100820e
ajs <140010746+aboodJS@users.noreply.github.com>
2025-10-24 21:38:36 +0300

    added a way to read and write to a file

--- lib/home.dart
+++ lib/home.dart
@@ -1,5 +1,7 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
-import 'package:shared_preferences/shared_preferences.dart';
+import 'package:path_provider/path_provider.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -25,13 +27,47 @@ class InputBox extends StatefulWidget {
 }
 
 class _InputBoxState extends State<InputBox> {
+  Future<String> get _localPath async {
+    final directory = await getApplicationDocumentsDirectory();
+
+    return directory.path;
+  }
+
+  showFile() async {
+    try {
+      final file = await _localFile;
+
+      // Read the file
+      final contents = await file.readAsString();
+
+      print(contents);
+    } catch (e) {
+      // If encountering an error, return 0
+      return 0;
+    }
+  }
+
+  Future<File> get _localFile async {
+    final path = await _localPath;
+    return File('$path/tasks.txt');
+  }
+
+  Future<File> writeTasks(List list) async {
+    final file = await _localFile;
+
+    // Write the file
+    return file.writeAsString('$list');
+  }
+
   addTodo() {
     setState(() {
       if (textController.text != "") {
         tasks = [...tasks, textController.text];
+        writeTasks(tasks);
         textController.text = '';
       }
     });
+    showFile();
   }
 
   removeTodo(taskKey) {
